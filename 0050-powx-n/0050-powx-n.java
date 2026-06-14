040c0d61a31afb68cb29e84ee9f08ddcaf8b9ae0
OmarAhmed0-dev <omarahmed0.dev@gmail.com>
2026-06-14 10:28:44 +0300

    Time: 0 ms (100%), Space: 42.4 MB (100%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,39 +1,8 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        //return binaryExp(x, (long) n);
-        if(n == 0) {
-            return 1;
-        }
-        if(n ==1 ) {
-            return x;
-        }
-        long nn = n;
-        double sum = 1;
-        if (nn < 0) {
-           nn = -nn;
-        }
-        while(nn > 0) {
-            if(nn % 2 == 0) {
-                x = x*x;
-                nn = nn/2;
-            }
-            else {
-              sum = sum *x;
-              nn--;
-            }
-        }
-        if(n <0) {
-            sum = 1.0/sum;
-        }
-       return sum;
-
-
-
-
-
+        return binaryExp(x, (long) n);
     }
-/*
+
     private double binaryExp(double x, long n) {
         if (n == 0) {
             return 1;
@@ -48,7 +17,5 @@ class Solution {
         } else {
             return binaryExp(x * x, n / 2);
         }
-        
     }
-    */
 }
